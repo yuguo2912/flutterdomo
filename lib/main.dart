cc1dbd814fd79a99b17792a49e8ab62d0865e001
Hugo Arnaudeau <arnaudeaudarsonval@gmail.com>
2025-03-28 21:29:11 +0100

    fix3

--- lib/main.dart
+++ lib/main.dart
@@ -601,7 +601,7 @@ class _ProfileIcon extends StatelessWidget {
                 value: Menu.itemTwo,
                 child: Text('Settings'),
               ),
-              const PopupMenuItem<Menu>( afgeagaega
+              const PopupMenuItem<Menu>(
                 value: Menu.itemThree,
                 child: Text('Sign Out'),
               ),
